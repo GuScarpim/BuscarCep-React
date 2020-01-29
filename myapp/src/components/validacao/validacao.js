19c1a870d1361f368b2f11361d72588e0c335798
Gustavo Scarpim <gustavoscarpim@gmail.com>
2020-01-28 21:16:51 -0300

    Terminando validações
    Colocando validações nos inputs

--- myapp/src/components/validacao/validacao.js
+++ myapp/src/components/validacao/validacao.js
@@ -13,26 +13,14 @@ export default class validacao extends Component {
       nome: '',
       senha: '',
       celular: '',
-      uf: '',
-      rg: '',
-      cpf: '',
-      cnpj: '',
+      url: '',
+      horario: '',
       data: '',
       email: '',
       error_nome: '',
       error_senha: '',
-      error_celular: '',
-      error_uf: '',
-      error_rg: '',
-      error_cpf: '',
-      error_cnpj: '',
-      error_data: ''
+      error_celular: ''
     }
-  }
-
-  componentDidMount() {
-    // let r = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])(?:([0-9a-zA-Z$*&@#])(?!\1)){8,}$/;
-    // console.log(r.test('Gu@198756'));
   }
 
   onChange = (e) => {
@@ -47,22 +35,6 @@ export default class validacao extends Component {
     }
   }
 
-  validarCpf = () => {
-    if (this.state.cpf.length <= 10) {
-      this.setState({ error_cpf: 'Insira todos os números do cpf' })
-    } else {
-      this.setState({ error_cpf: false })
-    }
-  }
-
-  validarCnpj = () => {
-    if (this.state.cnpj.length <= 13) {
-      this.setState({ error_cnpj: 'Insira todos os números do cnpj' })
-    } else {
-      this.setState({ error_cnpj: false })
-    }
-  }
-
   validarNome = () => {
     if (this.state.nome.length <= 2) {
       this.setState({ error_nome: 'Insira um nome válido' })
@@ -71,26 +43,13 @@ export default class validacao extends Component {
     }
   }
 
-  validarSenha = (e) => {
-    // let re = /(\d{3}|\d{2}|\(\d{2}\))\s*(\d{9}|\d{8})/;
-
-    // if (this.state.senha !== re) {
-    //   this.setState({ error_senha: 'A senha deve conter letras, número e caractere especial' })
-    // }
-    // else {
-    //   this.setState({ error_senha: false })
-    // }
-    this.setState({ senha: this.state.senha.replace(/([^\d\s/-])/g, '')})
-  }
-
-  validarRg = () => {
-    if (this.state.nome.length <= 2 || this.state.nome != String) {
-      this.setState({ error_nome: 'Insira um nome válido' })
+  validarSenha = () => {
+    if (this.state.senha.length <= 5) {
+      this.setState({ error_senha: 'Insira 5 números' })
     } else {
-      this.setState({ error_nome: false })
+      this.setState({ error_senha: false })
     }
   }
-
 
   render() {
     return <div className="container">
@@ -106,38 +65,34 @@ export default class validacao extends Component {
 
             <MDBInput label="E-mail" type="email" defaultValue={this.email} id="email" />
 
-            <MDBInput label="Celular" type="text" value={this.celular} onKeyUp={this.validarCell}
-              onChange={this.onChange} maxLength="12" id="celular" />
+            <MDBInput label="Celular" value={this.celular} onKeyUp={this.validarCell}
+              onChange={this.onChange} maxLength="12" id="celular" type="tel" />
             <span className="error">{this.state.error_celular}</span>
 
           </div>
           <div className="col-sm">
             <MDBInput label="Nome" value={this.nome} onKeyUp={this.validarNome}
-              onChange={this.onChange} maxLength="20" id="nome" />
+              onChange={this.onChange} maxLength="20" id="nome"
+              pattern="[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$" />
             <span className="error">{this.state.error_nome}</span>
 
-            <MDBInput label="CPF" value={this.cpf} onKeyUp={this.validarCpf}
-              onChange={this.onChange} maxLength="11" id="cpf" />
-            <span className="error">{this.state.error_cpf}</span>
+            <MDBInput label="CPF" value={this.cpf}
+              onChange={this.onChange} maxLength="11" id="cpf" type="number" />
 
-            <MDBInput label="CNPJ" value={this.cnpj} onKeyUp={this.validarCnpj}
-              onChange={this.onChange} maxLength="14" id="cnpj" />
-            <span className="error">{this.state.error_cnpj}</span>
+            <MDBInput label="Horário" value={this.cnpj} onKeyUp={this.validarHorario}
+              onChange={this.onChange} maxLength="14" id="horario" type="time" />
           </div>
 
           <div className="col-sm">
-            <MDBInput label="Senha" type="text" value={this.senha} onKeyUp={this.validarSenha}
-              onChange={this.onChange} maxLength="20" id="senha" />
+            <MDBInput label="Senha" type="password" value={this.senha}
+              onKeyUp={this.validarSenha} onChange={this.onChange} maxLength="20" id="senha" />
             <span className="error">{this.state.error_senha}</span>
-            {this.state.senha}
 
-            <MDBInput label="RG" value={this.rg} onKeyUp={this.validarRg}
-              onChange={this.onChange} maxLength="11" id="rg" />
-            <span className="error">{this.state.error_rg}</span>
+            <MDBInput label="URL" value={this.url} onKeyUp={this.validarUrl}
+              onChange={this.onChange} id="url" type="url" />
 
             <MDBInput label="Data" value={this.data} onKeyUp={this.validarData}
-              onChange={this.onChange} maxLength="11" id="data" />
-            <span className="error">{this.state.error_data}</span>
+              onChange={this.onChange} maxLength="11" id="data" type="date" />
 
             <button onSubmit={this.handleSubmit}> Botao</button>
           </div>
